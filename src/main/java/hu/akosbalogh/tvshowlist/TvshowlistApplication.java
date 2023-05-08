1ce245c3e0229a79f3cf35495b939d8052050f1d
Ákos Balogh <akosbalogh16@gmail.com>
2023-05-08 21:59:01 +0200

    Init.

---
+++ src/main/java/hu/akosbalogh/tvshowlist/TvshowlistApplication.java
@@ -0,0 +1,13 @@
+package hu.akosbalogh.tvshowlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvshowlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvshowlistApplication.class, args);
+	}
+
+}
